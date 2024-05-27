5e059a141ecfd92b11a2816b24f01c946de3b2d4
Datck <ckddat@gmail.com>
2024-05-27 13:31:27 +0700

    Update process.py
    make dict calculate

--- Process/process.py
+++ Process/process.py
@@ -1,3 +1,8 @@
+import numpy as np
+from scipy.spatial.distance import cdist
+
 def kmeans_init_center(X, k):
     #choose center randomly
     return X[np.random(X.shape[0], k, replace= False)]
+
+
